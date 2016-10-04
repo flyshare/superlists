793803e0b8ce80a21b1ca46b2fcc878743df13e3
FlyShare <flyshare@163.com>
2016-10-04 20:31:09 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,12 @@
 # _*_ coding:utf-8 _*_
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         """
         就算 test 出错,这两个方法也会照样执行
@@ -28,9 +30,38 @@ class NewVisitorTest(unittest.TestCase):
         # unittest 有很多这种断言函数:
         # assertEqual\assertTrue\assertFalse...
         self.assertIn('To-Do', self.browser.title)
+        head_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', head_text)
 
+        # 应用邀请她输入一个代表事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute(
+            'placeholder'), 'Enter a to-do item')
+
+        # 她在一个文本框中输入了 "Buy peacock feathers"
+        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        # # 这是 selenium 在输入框输入内容的方法
+        inputbox.send_keys('Buy peacock feathers')
+
+        # 她按回车键后,页面更新了
+        # 待办事项表格中显示了 "1. Buy peacock feathers"
+        # 不但可以输入内容,还可以发送 特殊的按键,如ENTER/CTRL等修改键
+        inputbox.send_keys('keys.ENTER')
+
+        table = self.browser.find_element_by_id('id_list_table')    # 找不到触发异常
+        rows = table.find_elements_by_tag_name('tr')    # 找不到返回一个 空 list
+        # any()生成器表达器,类似于 列表推导,但更出色
+        self.assertTrue(
+            any(row.text == '1. Buy peacock feathers' for row in rows))
+
+        # 页面中又显示了一个文本框,可以输入其他的待办事项
+        # 她输入了 "Use peacock feathers to make a fly"
+        # 伊迪丝做事很有条理
         # fail() 测试失败,生成错误信息
         self.fail('Finish the test!')
+
+        # 页面再次更新,她的清单中显示了这两个待办事项
+        [...]
 
 
 if __name__ == '__main__':
