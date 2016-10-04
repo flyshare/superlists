82aa2b7f4c1e62a74e21b238d24accd49d15ca50
FlyShare <flyshare@163.com>
2016-10-04 16:02:19 +0800

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,30 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page   # 在 views.py 中我们先 home_page,为了通过测试
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
+    """docstring for HomePageTest"""
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """
+        resolve() 是 Django内部函数,用于解析 url,并将其映射到相应的视图函数
+        检查解析网站根目录'/'时,是否能找到名为 home_page()函数
+        """
+        found = resolve('/')
+        # views 视图模块中的 home_page()函数
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """"""
+        request = HttpRequest()
+        # home_page()视图获取请求,返回响应.
+        response = home_page(request)
+        # response.content 是 原始字节,不是 字符串,因此使用 b''
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
