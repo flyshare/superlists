e9eaa4303182fe462b4c4313ab2e3a2e8aac3fd7
FlyShare <flyshare@163.com>
2016-10-05 12:33:47 +0800

    Model for list Items add associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    text = models.TextField(default='')   # 不使用 CharField 是因为 text不用设置长度
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 from lists.views import home_page   # 在 views.py 中我们先 home_page,为了通过测试
 
@@ -11,7 +12,7 @@ from lists.views import home_page   # 在 views.py 中我们先 home_page,为了
 class HomePageTest(TestCase):
     """docstring for HomePageTest"""
 
-    def test_root_url_resolves_to_home_page_view(self):
+    def _test_root_url_resolves_to_home_page_view(self):
         """
         resolve() 是 Django内部函数,用于解析 url,并将其映射到相应的视图函数
         检查解析网站根目录'/'时,是否能找到名为 home_page()函数
@@ -20,14 +21,14 @@ class HomePageTest(TestCase):
         # views 视图模块中的 home_page()函数
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def _test_home_page_returns_correct_html(self):
         """"""
         request = HttpRequest()
         # home_page()视图获取请求,返回响应.
         response = home_page(request)
         expected_html = render_to_string(
             'home.html', {'new_item_text': '1. Buy peacock feathers'})
-        #print(response.content.decode())
+        # print(response.content.decode())
         # decode() 把原始字符 转换成 Python 的 Unicode 字符串
         print(response.content.decode())
         print(expected_html)
@@ -38,10 +39,31 @@ class HomePageTest(TestCase):
         # self.assertTrue(response.content.endswith(b'</html>'))
         #
 
-    def test_home_page_can_save_a_POST_requet(self):
+    def _test_home_page_can_save_a_POST_requet(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = '1. Buy peacock feathers'
 
         response = home_page(request)
         self.assertIn('1. Buy peacock feathers', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    """"""
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_save_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_save_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
